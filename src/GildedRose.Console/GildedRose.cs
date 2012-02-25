09460ce0e47f5e8eb4e844d2f0428c94817db9c7
Arlo Belshee <arlo.belshee@live.com>
2012-02-25 11:59:23 -0800

    Now each method does only one thing.

--- src/GildedRose.Console/GildedRose.cs
+++ src/GildedRose.Console/GildedRose.cs
@@ -26,7 +26,7 @@
 	- "Conjured" items degrade in Quality twice as fast as normal items. Conjured is an adjective; it can
 		be applie to any item to make it conjured.
 
- * Feel free to make any changes to the UpdateQuality method and add any new code as long as everything
+ * Feel free to make any changes to the AgeItem method and add any new code as long as everything
  * still works correctly. However, do not alter the Item class or Items property as those belong to the
  * goblin in the corner who will insta-rage and one-shot you as he doesn't believe in shared code ownership
  * (we have kindly placed this code into HereBeDragons.cs, so that you won't accidentally change it. This
@@ -48,15 +48,19 @@ namespace GildedRose.Console
 		{
 			foreach(Item t in _innventory)
 			{
-				UpdateQuality(t);
+				AgeItem(t);
 			}
 		}
 
-		public static void UpdateQuality(Item item)
+		public static void AgeItem(Item item)
 		{
 			item.AgeOneDay();
+			UpdateQuality(item);
+		}
 
-			if (item.IsCheese())
+		private static void UpdateQuality(Item item)
+		{
+			if(item.IsCheese())
 			{
 				item.AdjustQuality(1);
 			}
