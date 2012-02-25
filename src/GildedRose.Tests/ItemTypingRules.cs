a86c4d8cad3dbaa17432104a3eb65588c8b97f01
Arlo Belshee <arlo.belshee@live.com>
2012-02-25 11:52:42 -0800

    I looked around and saw that I could easily write some unit tests for my new domain object's methods. So I wrote a couple of them.

---
+++ src/GildedRose.Tests/ItemTypingRules.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using GildedRose.Console;
+using FluentAssertions;
+
+namespace GildedRose.Tests
+{
+	[TestFixture]
+	public class ItemTypingRules
+	{
+		[Test]
+		public void ShouldRecognizeBrieAsCheese()
+		{
+			new Console.GildedRose.Item {Name = "Aged Brie"}.IsCheese().Should().BeTrue();
+		}
+
+		[Test]
+		public void ShouldRecognizeBackstagePassesAsTickets()
+		{
+			new Console.GildedRose.Item { Name = "Backstage passes to a TAFKAL80ETC concert" }.IsTickets().Should().BeTrue();
+		}
+	}
+}
---
+++ src/GildedRose.Tests/LegendaryItemsAreUnchanging.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using GildedRose.Console;
+using NUnit.Framework;
+
+namespace GildedRose.Tests
+{
+	[TestFixture]
+	public class LegendaryItemsAreUnchanging
+	{
+		[Test]
+		public void LegendaryItemQualityShouldNotBeAdjustable()
+		{
+			var testSubject = new Console.GildedRose.Item {Name = "Sulfuras, Hand of Ragnaros", Quality = 80};
+			testSubject.AdjustQuality(99);
+			testSubject.Quality.Should().Be(80);
+		}
+
+		[Test]
+		public void LegendaryItemQualityShouldNotBeSettabelToZero()
+		{
+			var testSubject = new Console.GildedRose.Item {Name = "Sulfuras, Hand of Ragnaros", Quality = 80};
+			testSubject.SetQualityToZero();
+			testSubject.Quality.Should().Be(80);
+		}
+
+		[Test]
+		public void LegendaryItemsShouldNotAge()
+		{
+			var testSubject = new Console.GildedRose.Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 3};
+			testSubject.AgeOneDay();
+			testSubject.SellIn.Should().Be(3);
+		}
+	}
+}
