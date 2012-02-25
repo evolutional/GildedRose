3bf92be6dd1763f28accd2be0803b0c1c58d8e16
Arlo Belshee <arlo.belshee@live.com>
2012-02-25 10:22:12 -0800

    I want to start testing the main update method. The first problem is that it operates by side effect, and also operates on a set at a time (which is the right algorithm, but more annoying for testing). So I'm going to extract a testable sub-method: the body of the loop.
    I'm doing this in several commits, so that you can see how I use the tools.
    
    First, extract method to remove the body of the loop. Let R# do all the defaults.

--- src/GildedRose.Console/GildedRose.cs
+++ src/GildedRose.Console/GildedRose.cs
@@ -48,14 +48,71 @@ namespace GildedRose.Console
 		{
 			for(int i = 0; i < _innventory.Count; i++)
 			{
-				if(_innventory[i].Name != "Aged Brie" && _innventory[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+				UpdateQuality(i);
+			}
+		}
+
+		public void UpdateQuality(int i)
+		{
+			if(_innventory[i].Name != "Aged Brie" && _innventory[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+			{
+				if(_innventory[i].Quality > 0)
 				{
-					if(_innventory[i].Quality > 0)
+					if(_innventory[i].Name != "Sulfuras, Hand of Ragnaros")
 					{
-						if(_innventory[i].Name != "Sulfuras, Hand of Ragnaros")
+						_innventory[i].Quality = _innventory[i].Quality - 1;
+					}
+				}
+			}
+			else
+			{
+				if(_innventory[i].Quality < 50)
+				{
+					_innventory[i].Quality = _innventory[i].Quality + 1;
+
+					if(_innventory[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+					{
+						if(_innventory[i].SellIn < 11)
 						{
-							_innventory[i].Quality = _innventory[i].Quality - 1;
+							if(_innventory[i].Quality < 50)
+							{
+								_innventory[i].Quality = _innventory[i].Quality + 1;
+							}
 						}
+
+						if(_innventory[i].SellIn < 6)
+						{
+							if(_innventory[i].Quality < 50)
+							{
+								_innventory[i].Quality = _innventory[i].Quality + 1;
+							}
+						}
+					}
+				}
+			}
+
+			if(_innventory[i].Name != "Sulfuras, Hand of Ragnaros")
+			{
+				_innventory[i].SellIn = _innventory[i].SellIn - 1;
+			}
+
+			if(_innventory[i].SellIn < 0)
+			{
+				if(_innventory[i].Name != "Aged Brie")
+				{
+					if(_innventory[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+					{
+						if(_innventory[i].Quality > 0)
+						{
+							if(_innventory[i].Name != "Sulfuras, Hand of Ragnaros")
+							{
+								_innventory[i].Quality = _innventory[i].Quality - 1;
+							}
+						}
+					}
+					else
+					{
+						_innventory[i].Quality = _innventory[i].Quality - _innventory[i].Quality;
 					}
 				}
 				else
@@ -63,58 +120,6 @@ namespace GildedRose.Console
 					if(_innventory[i].Quality < 50)
 					{
 						_innventory[i].Quality = _innventory[i].Quality + 1;
-
-						if(_innventory[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-						{
-							if(_innventory[i].SellIn < 11)
-							{
-								if(_innventory[i].Quality < 50)
-								{
-									_innventory[i].Quality = _innventory[i].Quality + 1;
-								}
-							}
-
-							if(_innventory[i].SellIn < 6)
-							{
-								if(_innventory[i].Quality < 50)
-								{
-									_innventory[i].Quality = _innventory[i].Quality + 1;
-								}
-							}
-						}
-					}
-				}
-
-				if(_innventory[i].Name != "Sulfuras, Hand of Ragnaros")
-				{
-					_innventory[i].SellIn = _innventory[i].SellIn - 1;
-				}
-
-				if(_innventory[i].SellIn < 0)
-				{
-					if(_innventory[i].Name != "Aged Brie")
-					{
-						if(_innventory[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-						{
-							if(_innventory[i].Quality > 0)
-							{
-								if(_innventory[i].Name != "Sulfuras, Hand of Ragnaros")
-								{
-									_innventory[i].Quality = _innventory[i].Quality - 1;
-								}
-							}
-						}
-						else
-						{
-							_innventory[i].Quality = _innventory[i].Quality - _innventory[i].Quality;
-						}
-					}
-					else
-					{
-						if(_innventory[i].Quality < 50)
-						{
-							_innventory[i].Quality = _innventory[i].Quality + 1;
-						}
 					}
 				}
 			}
