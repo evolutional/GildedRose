59ba474a4030ca702c9ec29352223ff0628386fa
Arlo Belshee <arlo.belshee@live.com>
2012-02-26 15:29:20 -0800

    Use data-driven tests to extract common code.

--- src/GildedRose.Tests/AdjustQuality.cs
+++ src/GildedRose.Tests/AdjustQuality.cs
@@ -14,36 +14,32 @@ namespace GildedRose.Tests
 			data.Quality.Should().Be(80);
 		}
 
-		[Test]
-		public void DecrementingNormalItemShouldReduceQualityByOne()
+		public object[][] DecrementQualityData = new[]
+			{
+				new object[] {30, 29},
+				new object[] {0, 0},
+			};
+
+		[Test, TestCaseSource("DecrementQualityData")]
+		public void DecrementingNormalItemShouldReduceQualityByOne(int initialQuality, int finalQuality)
 		{
-			var data = ItemWithQuality(30);
+			var data = ItemWithQuality(initialQuality);
 			Console.GildedRose.DecrementQuality(data);
-			data.Quality.Should().Be(29);
+			data.Quality.Should().Be(finalQuality);
 		}
 
-		[Test]
-		public void DecrementingNormalItemShouldNeverReduceQualityBelowZero()
-		{
-			var data = ItemWithQuality(0);
-			Console.GildedRose.DecrementQuality(data);
-			data.Quality.Should().Be(0);
-		}
+		public object[][] IncrementQualityData = new[]
+			{
+				new object[] {30, 31},
+				new object[] {50, 50},
+			};
 
-		[Test]
-		public void IncrementingNormalItemShouldIncreaseQualityByOne()
+		[Test, TestCaseSource("IncrementQualityData")]
+		public void IncrementingNormalItemShouldIncreaseQualityByOne(int initialQuality, int finalQuality)
 		{
-			var data = ItemWithQuality(30);
+			var data = ItemWithQuality(initialQuality);
 			Console.GildedRose.IncrementQuality(data);
-			data.Quality.Should().Be(31);
-		}
-
-		[Test]
-		public void IncrementingNormalItemShouldNeverImproveQualityAboveFifty()
-		{
-			var data = ItemWithQuality(50);
-			Console.GildedRose.IncrementQuality(data);
-			data.Quality.Should().Be(50);
+			data.Quality.Should().Be(finalQuality);
 		}
 
 		private static Console.GildedRose.Item ItemWithQuality(int quality)
