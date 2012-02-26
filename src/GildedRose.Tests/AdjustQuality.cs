6345a2b1a6bc5c70285bbf74b6db70afdb884b70
Arlo Belshee <arlo.belshee@live.com>
2012-02-26 15:19:54 -0800

    The two existing cases for decrement.

--- src/GildedRose.Tests/AdjustQuality.cs
+++ src/GildedRose.Tests/AdjustQuality.cs
@@ -13,5 +13,26 @@ namespace GildedRose.Tests
 			Console.GildedRose.DecrementQuality(data);
 			data.Quality.Should().Be(80);
 		}
+
+		[Test]
+		public void DecrementingNormalItemShouldReduceQualityByOne()
+		{
+			var data = ItemWithQuality(30);
+			Console.GildedRose.DecrementQuality(data);
+			data.Quality.Should().Be(29);
+		}
+
+		[Test]
+		public void DecrementingNormalItemShouldNeverReduceQualityBelowZero()
+		{
+			var data = ItemWithQuality(0);
+			Console.GildedRose.DecrementQuality(data);
+			data.Quality.Should().Be(0);
+		}
+
+		private static Console.GildedRose.Item ItemWithQuality(int quality)
+		{
+			return new Console.GildedRose.Item { Name = "Arbitrary item", Quality = quality };
+		}
 	}
 }
