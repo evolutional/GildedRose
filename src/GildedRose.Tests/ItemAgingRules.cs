8957ce8e7175ad3d39f5ccc857442ed780d0fc63
Arlo belshee <arlob@microsoft.com>
2012-10-02 17:22:20 -0700

    Give a decent name to the pinning test. This is ready to go as a starting point.

--- src/GildedRose.Tests/ItemAgingRules.cs
+++ src/GildedRose.Tests/ItemAgingRules.cs
@@ -2,24 +2,23 @@ using System.Text;
 using ApprovalTests;
 using ApprovalTests.Reporters;
 using NUnit.Framework;
-using FluentAssertions;
 
 namespace ConsoleApp.Tests
 {
 	[TestFixture]
 	public class ItemAgingRules
 	{
-		[Test, UseReporter(typeof(QuietReporter))]
-		public void ShouldAlwaysPass()
+		[Test, UseReporter(typeof (DiffReporter))]
+		public void PinDownTheZombie()
 		{
 			var testSubject = new GildedRose();
 			var log = new StringBuilder();
-			for(int i = 0; i < 50; i++)
+			for (var i = 0; i < 50; i++)
 			{
-				Dump(testSubject, log, i);
+				this.Dump(testSubject, log, i);
 				testSubject.UpdateQuality();
 			}
-			Dump(testSubject, log, 50);
+			this.Dump(testSubject, log, 50);
 			Approvals.Verify(log.ToString());
 		}
 
